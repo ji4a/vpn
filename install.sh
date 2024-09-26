71a760506100b9212ce44589671feba6919a8c33
ji4a <117786715+ji4a@users.noreply.github.com>
2024-09-26 14:15:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#
-# https://github.com/Nyr/openvpn-install
-#
-# Copyright (c) 2013 Nyr. Released under the MIT License.
-
 
 # Detect Debian users running the script with "sh" instead of bash
 if readlink /proc/$$/exe | grep -q "dash"; then
